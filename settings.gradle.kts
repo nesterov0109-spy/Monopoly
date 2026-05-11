6f63ed37eb73c0c80385007ece70adec5b106c5f
nesterovkos-arch <nesterovkos-arch@users.noreply.github.com>
2026-05-12 02:08:27 +0300

    Fix gradle config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,16 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
-
-dependencyResolution {
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "HermesAI"
 include(":app")
